272969703f3371d907122a0372d83a5728c67892
Athiyen <dhineesh2802@gmail.com>
2022-11-27 20:42:30 +0800

    copied the build dir from frontend

---
+++ build/precache-manifest.aae76d6f217f6870af98972b36793757.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac763c18619eb711d20a",
+    "url": "/static/css/main.e19a8238.chunk.css"
+  },
+  {
+    "revision": "ac763c18619eb711d20a",
+    "url": "/static/js/main.ac763c18.chunk.js"
+  },
+  {
+    "revision": "cc6feaca65f46ed04a31",
+    "url": "/static/js/1.cc6feaca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c516c171fba28d249acd8b5b0a134a0e",
+    "url": "/index.html"
+  }
+];
